1e69aedebc51ed3ca2ed7e0832d5292adab4dd6d
tessa <tessa@DESKTOP-7M78NHH>
2025-01-15 17:53:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Mission01;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string strnumrolls = "";
+        int numrolls = 0;
+        int[] sumList = new int[numrolls];
+        int partOfTotal = 0;
+        float percentOfTotal = 0;
+        int roundedPercent = 0;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n\n" +
+            "How many dice rolls would you like to simulate? ");
+
+        strnumrolls = System.Console.ReadLine();
+        numrolls = int.Parse(strnumrolls);
+
+        sumList = RollDice.Rolling(numrolls);
+
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS\n" +
+            "Each \"*\" represents 1% of the total number of rolls.\n" +
+            "Total number of rolls = " + numrolls + ". ");
+       
+
+        for (int i = 2; i < 13; i++)
+        {
+
+            partOfTotal = 0;
+
+            foreach (int sum in sumList)
+            {
+                if (sum == i)
+                {
+                    partOfTotal++;
+                }
+            
+                              
+            }
+            percentOfTotal = (float)partOfTotal / numrolls;
+            percentOfTotal = percentOfTotal * 100;
+
+            roundedPercent = (int)Math.Round(percentOfTotal);
+
+            System.Console.WriteLine(i + ":" + new string('*', roundedPercent));
+
+        }
+
+
+
+
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission01
+{
+    internal class RollDice
+    {
+        public static int[] Rolling(int numrolls)
+        {
+            // Create an instance of the Random class
+            Random rand = new Random();
+            int randomInt1 = 0;
+            int randomInt2 = 0;
+            int rollSum = 0;
+            int[] sumList = new int[numrolls];
+
+            for (int i = 0; i < numrolls; i++)
+            { // Generate a random integer between 1 and 6 (inclusive)
+                randomInt1 = rand.Next(1, 7);
+                randomInt2 = rand.Next(1, 7);
+
+                rollSum = randomInt1 + randomInt2;
+
+                sumList[i] = rollSum;
+            }
+
+            return sumList;
+        }
+    }
+}
